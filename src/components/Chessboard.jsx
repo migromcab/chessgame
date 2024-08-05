a8ca0d3e17bbdce6b4a1c5c9e70bdcacfdb156b5
migromcab <migromcab@icloud.com>
2024-08-05 15:25:34 +0200

    adding color

---
+++ src/components/Chessboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./ChessboardStyle.css";
+
+const AxisY = [1, 2, 3, 4, 5, 6, 7, 8];
+const AxisX = ["a", "b", "c", "d", "f", "g", "h"];
+
+export default function Chessboard() {
+  return <div className="chessboard"> </div>;
+}
