0f999ccce9de0b7a33928053874711c051b01171
migromcab <migromcab@icloud.com>
2024-08-05 16:04:15 +0200

    building axis logic

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./ChessboardStyle.css";
 
-const AxisY = [1, 2, 3, 4, 5, 6, 7, 8];
+const AxisY = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const AxisX = ["a", "b", "c", "d", "f", "g", "h"];
 
 export default function Chessboard() {
-  return <div className="chessboard"> </div>;
+  let board = [];
+
+  for (let j = AxisY.length - 1; j >= 0; j--) {
+    for (let i = 0; i < AxisX.length; j++) {
+      board.push(
+        <span>
+          {AxisX[i]} {AxisY[j]}
+        </span>
+      );
+    }
+  }
+
+  return <div className="chessboard"> {board} </div>;
 }
