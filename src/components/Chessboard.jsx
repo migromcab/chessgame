c9f51b58b69e8e441aa975400ed71c9e02d12148
migromcab <migromcab@icloud.com>
2024-08-05 16:50:31 +0200

    adding tiles color

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -9,18 +9,28 @@ export default function Chessboard() {
 
   for (let j = AxisY.length - 1; j >= 0; j--) {
     for (let i = 0; i < AxisX.length; i++) {
-      board.push(
-        <div className="tiles">
-          {AxisX[i]}
-          {AxisY[j]}
-        </div>
-      );
+      const number = i + j + 2;
+      if (number % 2 === 0) {
+        board.push(
+          <div className="white-tile">
+            {AxisX[i]}
+            {AxisY[j]}
+          </div>
+        );
+      } else {
+        board.push(
+          <div className="black-tile">
+            {AxisX[i]}
+            {AxisY[j]}
+          </div>
+        );
+      }
     }
   }
 
   return (
     <div className="chessboard">
-      <div className="table">{board}</div>
+      <div className="board">{board}</div>
     </div>
   );
 }
