11e566ceb08b46481fdb8b69606e09aa444e43ba
migromcab <migromcab@icloud.com>
2024-08-05 16:32:43 +0200

    creating rows

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Chessboard from "./components/Chessboard";
+import "./components/ChessboardStyle.css";
 
 function App() {
   return (
--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import "./ChessboardStyle.css";
 
 const AxisY = ["1", "2", "3", "4", "5", "6", "7", "8"];
-const AxisX = ["a", "b", "c", "d", "f", "g", "h"];
+const AxisX = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 export default function Chessboard() {
   let board = [];
 
   for (let j = AxisY.length - 1; j >= 0; j--) {
-    for (let i = 0; i < AxisX.length; j++) {
+    for (let i = 0; i < AxisX.length; i++) {
       board.push(
-        <span>
-          {AxisX[i]} {AxisY[j]}
-        </span>
+        <div className="tiles">
+          {AxisX[i]}
+          {AxisY[j]}
+        </div>
       );
     }
   }
 
-  return <div className="chessboard"> {board} </div>;
+  return (
+    <div className="chessboard">
+      <div className="table">{board}</div>
+    </div>
+  );
 }
