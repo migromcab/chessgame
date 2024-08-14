b056dba484796353b599a982308d70b31104c4f5
migromcab <migromcab@icloud.com>
2024-08-14 16:09:29 +0200

    sizing pieces

--- src/components/tile/tile.tsx
+++ src/components/tile/tile.tsx
@@ -24,13 +24,29 @@ export interface Props {
 function Tile({ number, image }: Props) {
   if (number % 2 === 0) {
     return (
-      <div className="tile black-tile">{image && <img src={image} />}</div>
+      <div className="tile black-tile">
+        {image && (
+          <div
+            style={{ backgroundImage: `url(${image})` }}
+            className="chess-piece"
+          ></div>
+        )}
+      </div>
     );
   } else {
     return (
-      <div className="tile white-tile">{image && <img src={image} />}</div>
+      <div className="tile white-tile">
+        {image && (
+          <div
+            style={{ backgroundImage: `url(${image})` }}
+            className="chess-piece"
+          ></div>
+        )}
+      </div>
     );
   }
 }
 
 export default Tile;
+
+//{image && <img src={image} />}
